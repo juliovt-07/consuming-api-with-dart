5859b365f743a733093839102de8eba620e7f94d
Júlio César <juliocesaar.ma@gmail.com>
2021-02-10 01:14:11 -0300

    consuming api

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,7 @@
-void main(List<String> args) {}
+import 'package:http/http.dart' as http;
+
+void main(List<String> args) async {
+  String url = 'https://calm-plains-33245.herokuapp.com/';
+  var response = await http.get(url);
+  print(response.body);
+}
